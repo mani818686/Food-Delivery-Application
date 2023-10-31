00e4d98918f3fa11a1cfc85f1cbbcabe3bf6a97f
Shekaju007 <shekharsanju97@gmail.com>
2023-10-30 19:57:31 -0500

    login page updated

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import './login.css';
 
 export default function Login() {
     const [userData,setuserData] = useState(
@@ -16,17 +17,20 @@ export default function Login() {
         console.log(userData)
       }
     return (
-        <div className='container mt-5 d-flex flex-column justify-content-center align-items-center border p-4'>
-            <h5 className='text-center'>Login Your Account</h5>
-            <div className="mb-3 col-4">
-                <label htmlFor="email" className="htmlForm-label">Email address</label>
-                <input type="email" className="htmlForm-control" id="email"  onChange={(e)=>handleData('email',e)} />
+        <div className='login-container'>
+            <h3 className='align'>Login</h3>
+            
+            <div className="form-group">
+                <label htmlFor="email" className="head">Email</label>
+                <input type="email" className="form-control" id="email"  onChange={(e)=>handleData('email',e)} />
             </div>
-            <div className="mb-3 col-4">
-                <label htmlFor="password" className="htmlForm-label">Password</label>
-                <input type="password" className="htmlForm-control" id="password" onChange={(e)=>handleData('password',e)}/>
+            <div className="form-group">
+                <label htmlFor="password" className="head">Password</label>
+                <input type="password" className="form-control" id="password" onChange={(e)=>handleData('password',e)}/>
             </div>
-            <button type="submit" className="btn btn-primary" onClick={handleLogin}>Login</button>
+            <button type="submit" className="btn" onClick={handleLogin}>Login</button>
+        
         </div>
+        
     )
 }
--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -1,10 +1,19 @@
 import React, { useState } from 'react'
+import "./register.css"
 
 export default function Register() {
   const [userData,setuserData] = useState(
     {"email":"",
-    "name":"",
-    "password":""
+    "firstName":"",
+    "lastName":"",
+    "password":"",
+    "Confirmpassword":"",
+    "phone":"",
+    "street":"",
+    "city":"",
+    "state":"",
+    "country":"",
+    "pin":""
   });
 
   const handleData = (column,e)=>{
@@ -17,21 +26,82 @@ export default function Register() {
     console.log(userData)
   }
   return (
-    <div className='container mt-5 d-flex flex-column justify-content-center align-items-center border p-4'>
-      <h5 className='text-center'>Create Account</h5>
-      <div className="mb-3 col-4">
-        <label htmlFor="email" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="email" onChange={(e)=>handleData('email',e)} />
-      </div>
-      <div className="mb-3 col-4">
-        <label htmlFor="name" className="form-label">Full Name</label>
-        <input type="text" className="form-control" id="name" onChange={(e)=>handleData('name',e)} />
-      </div>
-      <div className="mb-3 col-4">
-        <label htmlFor="password" className="form-label">Password</label>
-        <input type="password" className="form-control" id="password" onChange={(e)=>handleData('password',e)} />
-      </div>
-      <button type="submit" className="btn btn-primary" onClick={handleRegister}>Register</button>
+  <div class="container">
+    <div class="title mt-2 justify-content-center align-items-center border p-2">Registration</div>
+    <div class="content">
+        <div class="user-details">
+          <div class="input-box">
+            <span class="details">First Name</span>
+            <input type="text" placeholder="Enter your First name" onChange={(e)=>handleData('firstName',e)} required/>
+          </div>
+          <div class="input-box">
+            <span class="details">Last name</span>
+            <input type="text" placeholder="Enter your Last username" onChange={(e)=>handleData('lastName',e)} required/>
+          </div>
+          <div class="input-box">
+            <span class="details">Email</span>
+            <input type="email" placeholder="Enter your email" onChange={(e)=>handleData('email',e)} required/>
+          </div>
+          <div class="input-box">
+            <span class="details">Password</span>
+            <input type="password" placeholder="Enter your password" onChange={(e)=>handleData('password',e)} required/>
+          </div>
+          <div class="input-box">
+            <span class="details">Confirm Password</span>
+            <input type="password" placeholder="Confirm your password" onChange={(e)=>handleData('Confirmpassword',e)} required/>
+          </div>
+          <div class="input-box">
+            <span class="details">Phone Number</span>
+            <input type="tel" placeholder="Enter your number" onChange={(e)=>handleData('phone',e)} required/>
+          </div>  
+          <div class="input-box">
+            <span class="details">Street</span>
+            <input type="text" placeholder="Enter your street" onChange={(e)=>handleData('street',e)} required/>
+          </div> 
+          <div class="input-box">
+            <span class="details">City</span>
+            <input type="text" placeholder="Enter your City" onChange={(e)=>handleData('city',e)} required/>
+          </div>      
+          <div class="input-box">
+            <span class="details">State</span>
+            <input type="text" placeholder="Enter your State" onChange={(e)=>handleData('state',e)} required/>
+          </div> 
+          <div class="input-box">
+            <span class="details">Country</span>
+            <input type="text" placeholder="Enter your Country" onChange={(e)=>handleData('country',e)} required/>
+          </div> 
+          <div class="input-box">
+            <span class="details">PinCode</span>
+            <input type="text" placeholder="Enter your PinCode" onChange={(e)=>handleData('pin',e)} required/>
+          </div> 
+        </div>
+        {/* <div class="gender-details">
+          <input type="radio" name="gender" id="dot-1"/>
+          <input type="radio" name="gender" id="dot-2"/>
+          <input type="radio" name="gender" id="dot-3"/>
+          <span class="gender-title">Gender</span>
+          <div class="category">
+            <label for="dot-1">
+            <span class="dot one"></span>
+            <span class="gender">Male</span>
+          </label>
+          <label for="dot-2">
+            <span class="dot two"></span>
+            <span class="gender">Female</span>
+          </label>
+          <label for="dot-3">
+            <span class="dot three"></span>
+            <span class="gender">Prefer not to say</span>
+            </label>
+          </div>
+        </div> */}
+        <div class="button">
+          <input type="submit"  value="Register" onClick={handleRegister}/>
+        </div>
+
     </div>
+  </div>
+     
+      
   )
 }
