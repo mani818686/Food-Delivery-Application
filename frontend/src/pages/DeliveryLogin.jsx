de96f8e1271b5bcedc09f4573df38f4d6ee5fd4d
Shekaju007 <shekharsanju97@gmail.com>
2023-11-22 21:26:46 -0600

    AllDeliveryPersonDataSave

---
+++ frontend/src/pages/DeliveryLogin.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import './login.css';
+import { useDispatch } from 'react-redux';
+import { saveUser } from '../userslice';
+import {postData} from '../http-post-service';
+import { useNavigate } from 'react-router-dom';
+import { setAuthToken, setLoggedIn, setisAdmin } from '../loginSlice';
+import { addAllproducts } from '../cartslice';
+
+
+export default function Login() {
+  const navigate = useNavigate();
+const dispatch = useDispatch();
+const defaultUser = { "email": "", "password": "" }
+ 
+  const [userData, setuserData] = useState(defaultUser);
+
+  const handleData = (column, e) => {
+    setuserData((data) => ({
+      ...data,
+      [column]: e.target.value
+    }))
+  }
+  const handledeliveryLogin = async () => {
+    const response = await postData("/deliveryInfo/deliverylogin", JSON.stringify(userData))
+    if (response.message == "Delivery Auth successful") {
+      setuserData(defaultUser)
+      dispatch(saveUser(response.userDetails))
+      dispatch(setLoggedIn(true))
+      dispatch(setAuthToken(response.token))
+      dispatch(addAllproducts(response.userDetails.wishlist))
+      localStorage.setItem("userLoggedIn", true);
+      localStorage.setItem("authToken", response.token);
+      navigate("/")
+    }
+    else {
+      console.error(response)
+      setuserData(defaultUser)
+    }
+  }
+   
+  
+
+  return (
+    <div className='login-container'>
+      <h3 className='align'>Login</h3>
+
+      <div className="form-group">
+        <label htmlFor="email" className="head">Email</label>
+        <input type="email" className="form-control-item" id="email" onChange={(e) => handleData('email', e)} />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password" className="head">Password</label>
+        <input type="password" className="form-control-item" id="password" onChange={(e) => handleData('password', e)} />
+      </div>
+      <button type="submit" className="btn-color" onClick={handledeliveryLogin}>Login</button>
+
+    </div>
+
+  )
+  }
---
+++ frontend/src/pages/DeliveryRegister.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react'
+import "./DeliveryRegister.css"
+import {postData} from '../http-post-service';
+import { useNavigate } from 'react-router-dom';
+
+export default function Register() {
+  const defaultUser = {"email":"",
+  "firstName":"",
+  "lastName":"",
+  "password":"",
+  "Confirmpassword":"",
+  "phoneNumber":"",
+  // "street":"",
+  // "city":"",
+  // "state":"",
+  // "country":"",
+  // "pincode":""
+}
+  const [userData,setuserData] = useState(defaultUser);
+  const navigate = useNavigate();
+
+  const handleData = (column,e)=>{
+    setuserData((data)=>({
+      ...data,
+      [column]:e.target.value
+    }))
+  }
+  const handleDeliveryRegister = async() =>{
+    console.log(userData)
+      const response = await postData("/deliveryInfo/deliverysignup",JSON.stringify(userData))
+      if(response.message == "delivery customer createdd"){
+        setuserData(defaultUser)
+        navigate("/")
+      }
+      else{
+        console.error(response)
+        setuserData(defaultUser)
+      }
+  }
+  return (
+  <div class="container">
+    <div class="title">Registration</div>
+    <div class="content">
+        <div class="user-details">
+          <div class="input-box">
+            <span class="details">First Name</span>
+            <input type="text" placeholder="Enter your First name" onChange={(e)=>handleData('firstName',e)} required/>
+          </div>
+          <div class="input-box">
+            <span class="details">Last name</span>
+            <input type="text" placeholder="Enter your Last username" onChange={(e)=>handleData('lastName',e)} required/>
+          </div>
+          <div class="input-box">
+            <span class="details">Email</span>
+            <input type="email" placeholder="Enter your email" onChange={(e)=>handleData('email',e)} required/>
+          </div>
+          <div class="input-box">
+            <span class="details">Password</span>
+            <input type="password" placeholder="Enter your password" onChange={(e)=>handleData('password',e)} required/>
+          </div>
+          <div class="input-box">
+            <span class="details">Confirm Password</span>
+            <input type="password" placeholder="Confirm your password" onChange={(e)=>handleData('Confirmpassword',e)} required/>
+          </div>
+          <div class="input-box">
+            <span class="details">Phone Number</span>
+            <input type="tel" placeholder="Enter your number" onChange={(e)=>handleData('phoneNumber',e)} required/>
+          </div>  
+          {/* <div class="input-box">
+            <span class="details">Street</span>
+            <input type="text" placeholder="Enter your street" onChange={(e)=>handleData('street',e)} required/>
+          </div> 
+          <div class="input-box">
+            <span class="details">City</span>
+            <input type="text" placeholder="Enter your City" onChange={(e)=>handleData('city',e)} required/>
+          </div>      
+          <div class="input-box">
+            <span class="details">State</span>
+            <input type="text" placeholder="Enter your State" onChange={(e)=>handleData('state',e)} required/>
+          </div> 
+          <div class="input-box">
+            <span class="details">Country</span>
+            <input type="text" placeholder="Enter your Country" onChange={(e)=>handleData('country',e)} required/>
+          </div> 
+          <div class="input-box">
+            <span class="details">PinCode</span>
+            <input type="text" placeholder="Enter your PinCode" onChange={(e)=>handleData('pincode',e)} required/>
+          </div>  */}
+        </div>
+        <div class="button">
+          <input type="submit"  value="Register" onClick={handleDeliveryRegister}/>
+        </div>
+
+    </div>
+  </div>
+     
+      
+  )
+}
